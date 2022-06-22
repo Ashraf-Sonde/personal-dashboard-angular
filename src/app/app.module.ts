68e5e6f16991c710ba83c2bdf797148511c30ce1
Ashraf-Sonde <ashrafsonde@gmail.com>
2022-06-23 01:55:58 +0530

    added and configured edit note component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { BookmarkTileComponent } from './bookmark-tile/bookmark-tile.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AddNoteComponent } from './add-note/add-note.component';
 import { NoteCardComponent } from './note-card/note-card.component';
+import { EditNoteComponent } from './edit-note/edit-note.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { NoteCardComponent } from './note-card/note-card.component';
     NotesComponent,
     BookmarkTileComponent,
     AddNoteComponent,
-    NoteCardComponent
+    NoteCardComponent,
+    EditNoteComponent
   ],
   imports: [
     BrowserModule,
