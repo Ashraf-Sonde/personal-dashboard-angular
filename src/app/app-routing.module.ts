4074b727035106e60096cd46d9832b3dc77dd7e5
Ashraf-Sonde <ashrafsonde@gmail.com>
2022-06-23 14:46:59 +0530

    added add-todo form and functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import {TodosComponent} from './todos/todos.component';
 import {NotesComponent} from './notes/notes.component';
 import {AddNoteComponent} from './add-note/add-note.component';
 import {EditNoteComponent} from './edit-note/edit-note.component';
+import {AddTodoComponent} from './add-todo/add-todo.component';
 
 const routes: Routes = [
   { path: 'bookmarks', component: BookmarksComponent, data: { tabIndex: 1}},
   { path: 'todos', component: TodosComponent, data: { tabIndex: 2}},
+  { path: 'todos/add', component: AddTodoComponent},
   { path: 'notes', component: NotesComponent, data: { tabIndex: 3}},
   { path: 'notes/add', component: AddNoteComponent},
   { path: 'notes/:id', component: EditNoteComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AddNoteComponent } from './add-note/add-note.component';
 import { NoteCardComponent } from './note-card/note-card.component';
 import { EditNoteComponent } from './edit-note/edit-note.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
     AddNoteComponent,
     NoteCardComponent,
     EditNoteComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule,
