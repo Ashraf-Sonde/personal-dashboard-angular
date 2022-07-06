915e83d9528ec49b895b8d3c3c941cb3733f47ac
Ashraf-Sonde <ashrafsonde@gmail.com>
2022-07-06 15:55:52 +0530

    done the edit todo feature and form

--- src/app/edit-todo/edit-todo.component.ts
+++ src/app/edit-todo/edit-todo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {TodoService} from '../shared/todo.service';
+import {Todo} from '../shared/todo.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-todo',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTodoComponent implements OnInit {
 
-  constructor() { }
+  todo: Todo
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private todoService: TodoService
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const todoId = paramMap.get('id');
+      // @ts-ignore
+       this.todo = this.todoService.getTodo(todoId);
+    })
+  }
+
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) return
+
+    this.todoService.updateTodo(this.todo.id, form.value);
+    this.router.navigateByUrl('/todos');
   }
 
 }
